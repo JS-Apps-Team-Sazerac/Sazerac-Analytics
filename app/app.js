d1fc7438ebda9becf96ec025c516625555b0bbaa
Boray <boray@abv.bg>
2015-08-31 12:34:14 +0300

    toggle "#sidebar-left" now work

--- app/app.js
+++ app/app.js
@@ -1,4 +1,16 @@
+/// <reference path="../lib/jquery-2.1.4.js" />
+
+'use strict'
 
 import $ from 'jquery';
 
-console.log($);
+$(document).ready(function () {
+
+    $('.show-sidebar').on('click', function (e) {
+        e.preventDefault();
+        $('div#main').toggleClass('sidebar-show');
+    });
+      
+
+    
+});
